56fce5e6c747139c84d9d365721de7162f7bbf76
connor stokes <cstokes@d225.bbn.com>
2015-08-07 10:34:29 -0400

    Updated headers

--- adept-api/src/main/java/adept/common/AudioFileType.java
+++ adept-api/src/main/java/adept/common/AudioFileType.java
@@ -18,18 +18,6 @@
 * -------
 */
 
-/*******************************************************************************
- * Raytheon BBN Technologies Corp., November 2013
- * 
- * THIS CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS
- * OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * Copyright 2013 Raytheon BBN Technologies Corp.  All Rights Reserved.
- ******************************************************************************/
-/*
- * 
- */
 package adept.common;
 
 
